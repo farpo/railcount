ef307e102e44c926b5e9dc7a9877bc09293101f8
farpo <77583105+farpo@users.noreply.github.com>
2023-11-13 17:19:52 +0100

    kl

--- build/behavior_packs/railcount/scripts/main.js
+++ build/behavior_packs/railcount/scripts/main.js
@@ -143,12 +143,12 @@ function mainTick() {
             world.getDimension("overworld").runCommandAsync("say Hello starter!");
             world.beforeEvents.chatSend.subscribe(async (eventData) => {
                 const p = eventData.sender;
-                if (eventData.message === "po") {
+                if (eventData.message === "start") {
                     counter = new RailIterator(p.location.x, p.location.y, p.location.z, p.dimension);
                     world.sendMessage(counter.run(eventData.sender).toString());
                     world.sendMessage("op");
                 }
-                else if (eventData.message === "co") {
+                else if (eventData.message === "c") {
                     world.sendMessage(counter.run(eventData.sender).toString());
                     world.sendMessage("op");
                     checkStateAndLoc(eventData.sender);
